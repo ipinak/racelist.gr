f4aadde631a92f8612372114f1bd6f13066c0742
ipinak <ipinak@users.noreply.github.com>
2026-03-18 07:59:31 +0000

    deploy: bffdd6c19a966811de194bf8a9586eb950bd4731

--- _nuxt/CQ8NpSs7.js
+++
@@ -1,1 +0,0 @@
-const o=t=>{const e={year:"numeric",month:"2-digit",day:"2-digit"};return new Date(t).toLocaleDateString("el-GR",e)};export{o as f};
---
+++ _nuxt/DSrDMWj5.js
@@ -0,0 +1,1 @@
+const n=window.setInterval,a=t=>{const e={year:"numeric",month:"2-digit",day:"2-digit"};return new Date(t).toLocaleDateString("el-GR",e)};export{a as f,n as s};
